81bc73a78131da6d0e79c928a7c8edebd54a710a
daniela guzman <danielaguzman50@outlook.es>
2025-05-29 12:31:47 -0500

    se agrego modal y cambio el css

--- OnePiece/onePiece.js
+++ OnePiece/onePiece.js
@@ -1,3 +1,41 @@
+function createModalStructure() {
+  const modal = document.createElement("div");
+  modal.id = "modal";
+  // No hay estilos aquí
+
+  const modalContent = document.createElement("div");
+  modalContent.id = "modalContent";
+
+  const closeButton = document.createElement("span");
+  closeButton.id = "closeButton";
+  closeButton.innerHTML = "&times;";
+  closeButton.onclick = function () {
+    modal.classList.remove("show"); // quitar clase para ocultar modal
+  };
+
+  modalContent.appendChild(closeButton);
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+}
+
+createModalStructure();
+
+function showModal(text) {
+  const modal = document.getElementById("modal");
+  const content = document.getElementById("modalContent");
+
+  // Limpia todo menos el botón cerrar
+  while (content.childNodes.length > 1) {
+    content.removeChild(content.lastChild);
+  }
+
+  const textTag = document.createElement("p");
+  textTag.innerHTML = text;
+  content.appendChild(textTag);
+
+  modal.classList.add("show"); // mostrar modal agregando clase
+}
+
 //cambiamos nombre de las variables a ingles
 function showTitle(title, subtitle) {
   let chapter = document.getElementById("title"); // Busca el div con id="title"
@@ -24,13 +62,13 @@ function pintarEpisodio(title, image, text, containerId) {
   titleTag.innerHTML = title;
   let imageTag = document.createElement("img");
   imageTag.src = image;
-  let textTag = document.createElement("p");
-  textTag.innerHTML = text;
+  imageTag.onclick = () => showModal(text);
 
   container.appendChild(titleTag);
   container.appendChild(imageTag);
-  container.appendChild(textTag);
 }
+
+//pasar data a la funcion pintarEpisodio
 pintarEpisodio(
   "¡Dime que quieres vivir!¡Somos nakamas!",
   "assets/Img Robin.jpg",
@@ -61,77 +99,3 @@ pintarEpisodio(
   "Ace es golpeado por el puño de magma del almirante Akainu como un intento de defender a Luffy de un ataque mortal para él, cayendo así en los brazos de su hermano. Ace piensa en su pasado y recuerda su objetivo en la vida: Saber si merecía o no haber nacido. Akainu procede a atacar a Luffy, pero Jinbe detiene este ataque, mientras tanto, Marco no puede quitarse las esposas, por lo que buscan a Galdino, quien se las quita para que vaya, junto a Vista, a ayudar a Luffy, golpeando a Akainu, sin embargo, se dan cuenta de que son ineficaces contra el almirante.Se puede ver a todo el mundo en estado de shock ante las heridas de Ace, y, al deducir que sus órganos fueron quemados sin posibilidad de cura, Ace se da cuenta de que su vida terminará pronto, por ésto, decide darle las gracias a Luffy y Sabo por ser sus hermanos, así como pedirle al primero que fuera su portador de voz, pues la suya no era lo suficientemente fuerte como para llegar a los oídos de las personas a quienes quería agradecer.",
   "chapter6"
 );
-
-/* function episode1 (){
-  let chapter = document.getElementById("chapter2")
-  let title2 = document.createElement("h2")
-  const image1= document.createElement("img")
-  let par =document.createElement("p") 
-  title2.innerHTML = "¡Dime que quieres vivir!¡Somos nakamas!";
-  image1.src="assets/Img Robin.jpg"
-  par.textContent= "Ohara es destruida por completo y gracias al sacrificio de Saul, Robin escapa y le dan una recompensa de Belly79.000.000 y termina su historia, si bien considera que los Sombrero de Paja eran sus amigos, ella cree que un día, ya no será capaz de protegerlos de sus perseguidores, y la traicionaran a ella como los demás lo hicieron. Después de que Spandam señala que teniendo a Robin significa declarar la guerra a todo el mundo, Luffy dice a Sogeking que queme la bandera del Gobierno Mundial. Sogeking usa su Hi no Tori Boshi / Fire Bird Star para quemar la bandera, y se declara la guerra al mundo por el bien de Robin. Robin, conmovida por la profundidad de la acción de Luffy, grita que quiere vivir y volver a la mar con Luffy, él y los otros se preparan para la batalla con el CP9 y salvar a Robin."
-  chapter.appendChild(title2);
-  chapter.appendChild(image1);
-  chapter.appendChild(par);
-}
-episode1();
-
-function episode2 (){
-  let chapter = document.getElementById("chapter3")
-  let title3 = document.createElement("h2")
-  const image2= document.createElement("img")
-  let par1 = document.createElement("p")
-  title3.innerHTML = "¡Los sentimientos mostrados a través de los puños! Luffy ejecuta el gattling a la maxima potencia";
-  image2.src="assets/luffy.png"
-  par1.innerHTML= "Los Piratas del Sombrero de Paja continuan su lucha contra los infantes de marina, mientras que Usopp se revela a Luffy y lo alienta para que pueda continuar luchando. Zoro tiene su Yubashiri destruida por Shu , un capitan de la marina usuario de la fruta Sabi Sabi no Mi. Luffy recuerda lo que hizo Robin por ellos y Luffy derrota a Rob Lucci con su poderoso ataque, Gomu Gomu no Jet Gatling, que rompe Lucci través de la pared y asombra a todos los infantes de marina que se enteran de lo que acaba de suceder."
-  chapter.appendChild(title3);
-  chapter.appendChild(image2);
-  chapter.appendChild(par1);
-}
-episode2();
-
-function episode3 (){
-  let chapter = document.getElementById("chapter4")
-  let title4 = document.createElement("h2")
-  const image3= document.createElement("img")
-  let par2 = document.createElement("p")
-  title4.innerHTML = "¡Gracias Merry! La nevada en el mar de las despedidas";
-  image3.src="assets/merry.png"
-  par2.innerHTML= "En cuanto a la tripulación hace su camino de regreso a Water 7, se reúnen con toda la compañía Galley-La y Iceburg. El Going Merry se parte en 2 y ahora ya no tiene reparación ya que la quilla se parte completamente, entonces la tripulación le da un funeral vikingo quemándolo, ya que el fondo del mar es oscuro y silencioso. Luego el barco pide disculpas por no poder navegar mas con ellos, Luffy dice que los que tienen que disculparse son ellos porque han hecho cosas que terminaron dañándolo, a lo que el Merry responde que a pesar de eso él fue muy feliz junto a ellos y muere..."
-  chapter.appendChild(title4);
-  chapter.appendChild(image3);
-  chapter.appendChild(par2);
-}
-episode3();
-
-function episode4 (){
-  let chapter = document.getElementById("chapter5")
-  let title5 = document.createElement("h2")
-  const image4= document.createElement("img")
-  let par3 = document.createElement("p")
-  title5.innerHTML = "El final de la travesia. Abrir la puerta de la justicia";
-  image4.src="assets/bon clay (2).jpg"
-  par3.innerHTML= "Los presos huyen de Magellan, quien pide un barco para perseguirlos. Se salvan gracias a los tiburones ballena que llamó Jinbe, que los impulsan hasta el buque, aunque son atacados por los barcos de los contraalmirantes que aún quedan en las aguas de Impel Down. Los disparos comienzan a dañar a la nave, por lo que Crocodile decide contraatacar. Mientras tanto, Jinbe está al mando del barco, dirigiéndose a la puerta de la justicia, a pesar de que está cerrada. Luffy se da cuenta de la ausencia de Mr. 2. Jinbe le dice que no esperaba contarle la verdad hasta que pasaran la puerta de la justicia, cuando el Den Den Mushi perdiera la cobertura. Luffy le exige que le diga por qué le ha dejado ir. Entonces se descubre que era una idea del propio Mr. 2, quien ayudaría a Luffy de la misma manera que hizo en Arabasta: sacrificándose y luchando con un enemigo más fuerte que él para que Luffy pudiera huir y conseguir su objetivo."
-  chapter.appendChild(title5);
-  chapter.appendChild(image4);
-  chapter.appendChild(par3);
-}
-episode4(); */
-/*
-function episode5 (){
-  let chapter = document.getElementById("chapter6")
-  let title6 = document.createElement("h2") 
-  const image5 = document.createElement("img")
-  let par4 = document.createElement("p")
-  title6.innerHTML = "¡Buscando la respuesta! ¡Muere Ace Puño de Fuego!";
-  image5.src="assets/ace.jpg"
-  par4.innerHTML= "Ace es golpeado por el puño de magma del almirante Akainu como un intento de defender a Luffy de un ataque mortal para él, cayendo así en los brazos de su hermano. Ace piensa en su pasado y recuerda su objetivo en la vida: Saber si merecía o no haber nacido. Akainu procede a atacar a Luffy, pero Jinbe detiene este ataque, mientras tanto, Marco no puede quitarse las esposas, por lo que buscan a Galdino, quien se las quita para que vaya, junto a Vista, a ayudar a Luffy, golpeando a Akainu, sin embargo, se dan cuenta de que son ineficaces contra el almirante.Se puede ver a todo el mundo en estado de shock ante las heridas de Ace, y, al deducir que sus órganos fueron quemados sin posibilidad de cura, Ace se da cuenta de que su vida terminará pronto, por ésto, decide darle las gracias a Luffy y Sabo por ser sus hermanos, así como pedirle al primero que fuera su portador de voz, pues la suya no era lo suficientemente fuerte como para llegar a los oídos de las personas a quienes quería agradecer."
-  chapter.appendChild(title6);
-  chapter.appendChild(image5);
-  chapter.appendChild(par4);
-}
-episode5();
-*/
-/*title = document.createComment("h1");
-title.textContent = "One Piece";
-document.body.appendChild(title);*/
